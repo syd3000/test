2205a376bd801bd7664d59a6cbb1ea0ab9c7cc8a
esappesapp <pielsydney@gmail.com>
2025-03-17 13:43:28 -0400

    idk

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://syd3000.github.io',
+    base: 'test',
+});
