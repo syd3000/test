5c301731e26bb7bf214f6c7fbaa458f91c280206
esappesapp <pielsydney@gmail.com>
2025-03-17 14:56:07 -0400

    changing url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://syd3000.github.io',
-    base: '/test',
+    site: 'https://sydneypiel.com',
 });
